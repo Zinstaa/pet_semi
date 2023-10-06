d518ea3dbb0f5b503cc7bb99c9e869c6a64aa906
checw0528 <146050806+checw0528@users.noreply.github.com>
2023-10-06 17:13:05 +0900

    자유게시판 파일업로드 - 채찬욱

---
+++ Pet_Semi/src/com/kh/pet/common/MyFileRenamePolicy.java
@@ -0,0 +1,27 @@
+package com.kh.pet.common;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
+public class MyFileRenamePolicy implements FileRenamePolicy {
+	
+	@Override
+	public File rename(File originFile) {
+		
+		String originName = originFile.getName();
+		
+		String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		
+		int randomNum = (int)(Math.random() * 90000) + 10000;
+		
+		String ext = originName.substring(originName.lastIndexOf("."));
+		
+		String changeName = "PET_" + currentTime + "_" + randomNum + ext;
+		
+		return new File(originFile.getParent(), changeName);
+	}
+	
+}
