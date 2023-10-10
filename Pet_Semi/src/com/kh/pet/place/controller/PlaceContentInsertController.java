0664349d3855092e321cebe5a03038d26b5e94c4
kstae19 <85491369+kstae19@users.noreply.github.com>
2023-10-10 14:49:35 +0900

    Create PlaceContentInsertController.java

---
+++ Pet_Semi/src/com/kh/pet/place/controller/PlaceContentInsertController.java
@@ -0,0 +1,80 @@
+package com.kh.pet.place.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.kh.pet.common.MyFileRenamePolicy;
+import com.kh.pet.place.model.vo.Place;
+import com.oreilly.servlet.MultipartRequest;
+
+/**
+ * Servlet implementation class PlaceContentInsertController
+ */
+@WebServlet("/insert.pl")
+public class PlaceContentInsertController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PlaceContentInsertController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			
+			int maxSize = 1024 * 1024 * 10;
+			
+			String savePath = request.getServletContext().getRealPath("resources/place_upfiles/");
+			
+			MultipartRequest multiRequest = new MultipartRequest(request, savePath, maxSize, "UTF-8", new MyFileRenamePolicy());
+			
+			int placeCategory = Integer.parseInt(multiRequest.getParameter("placeCategoryNo"));
+			int placeLocal = Integer.parseInt(multiRequest.getParameter("localCategoryNo"));
+			String placeName = multiRequest.getParameter("placeName");
+			String placeInfo = multiRequest.getParameter("placeInfo");
+			String placeAround = multiRequest.getParameter("placeAround");
+			String placePrice = multiRequest.getParameter("placePrice");
+			String placeCaution = multiRequest.getParameter("placeCaution");
+			String placeMap = multiRequest.getParameter("placeMap");
+			int userNo = Integer.parseInt(multiRequest.getParameter("memberNo"));
+			
+			Place p = new Place();
+			p.setPlaceCategoryNo(placeCategory);
+			p.setLocalCategoryNo(placeLocal);
+			p.setPlaceName(placeName);
+			p.setPlaceInfo(placeInfo);
+			p.setPlaceAround(placeAround);
+			p.setPlacePrice(placePrice);
+			p.setPlaceCaution(placeCaution);
+			p.setPlaceMap(placeMap);
+			p.setMemberNo(userNo);
+		}
+	
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
