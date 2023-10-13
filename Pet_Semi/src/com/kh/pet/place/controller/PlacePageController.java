3e3abde4992729ff44efb1f516e29d69dbc53d2c
kstae19 <85491369+kstae19@users.noreply.github.com>
2023-10-13 16:37:49 +0900

    Update PlacePageController.java

--- Pet_Semi/src/com/kh/pet/place/controller/PlacePageController.java
+++ Pet_Semi/src/com/kh/pet/place/controller/PlacePageController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.kh.pet.place.model.service.PlaceService;
 import com.kh.pet.place.model.vo.Place;
+import com.kh.pet.place.model.vo.PlacePageInfo;
 
 /**
  * Servlet implementation class PlacePageController
@@ -32,10 +33,41 @@ public class PlacePageController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		int listCount; 
+		int currentPage;
+		int pageLimit; 
+		int placeLimit;
+		  
+		int maxPage; 
+		int startPage;
+		int endPage;
+		
+		listCount = new PlaceService().selectListCount();
+		
+		currentPage = Integer.parseInt(request.getParameter("ppage"));
+		
+		pageLimit = 10;
+	      
+        placeLimit = 9;
+        
+        maxPage = (int)Math.ceil((double)listCount / placeLimit);
+        
+        startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
+        
+        endPage = startPage + pageLimit - 1;
+        
+        if(endPage > maxPage) {
+            endPage = maxPage;
+         }
+        
+        PlacePageInfo ppi = new PlacePageInfo(listCount, currentPage, pageLimit,
+                placeLimit, maxPage, startPage, endPage);
+		
 		// 화면에 표시하기 이전 상태 => 테이블로부터 조회
-		ArrayList<Place> list = new PlaceService().selectPlaceContentList();
+		ArrayList<Place> list = new PlaceService().selectPlaceContentList(ppi);
 		
 		request.setAttribute("list", list);
+		request.setAttribute("ppi", ppi);
 		request.getRequestDispatcher("views/place/placePage.jsp").forward(request, response);
 	}
 
