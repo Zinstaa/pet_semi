f1e53d9bcc6d6ece883cb6cdd4a6a10a26abfd00
kstae19 <85491369+kstae19@users.noreply.github.com>
2023-10-13 16:37:59 +0900

    Update PlaceContentEnrollFormController.java

--- Pet_Semi/src/com/kh/pet/place/controller/PlaceContentEnrollFormController.java
+++ Pet_Semi/src/com/kh/pet/place/controller/PlaceContentEnrollFormController.java
@@ -1,11 +1,17 @@
 package com.kh.pet.place.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.pet.place.model.service.PlaceService;
+import com.kh.pet.place.model.vo.LocalCategory;
+import com.kh.pet.place.model.vo.PlaceCategory;
 
 /**
  * Servlet implementation class PlaceContentEnrollFormController
@@ -26,6 +32,11 @@ public class PlaceContentEnrollFormController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<PlaceCategory> pllist = new PlaceService().selectPlaceCategoryList();
+		ArrayList<LocalCategory> lolist = new PlaceService().selectLoaclCategoryList();
+		
+		request.setAttribute("pllist", pllist);
+		request.setAttribute("lolist", lolist);
 		
 		request.getRequestDispatcher("views/place/placeContentEnrollForm.jsp").forward(request, response);
 	
@@ -40,3 +51,4 @@ public class PlaceContentEnrollFormController extends HttpServlet {
 	}
 
 }
+
